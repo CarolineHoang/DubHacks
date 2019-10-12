6a9634c1350c1e8a3bf2f974da525e87d69d4df0
Cremebrutale <caroline.hoang@columbiaspectator.com>
2019-10-12 16:51:59 -0700

    Updates

---
+++ precache-manifest.d3525eeaa71cd3bddfcd7be0071283a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe609288af138eaf75347464493902aa",
+    "url": "/DubHacks/index.html"
+  },
+  {
+    "revision": "d660a5eb8e578da025cc",
+    "url": "/DubHacks/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ee060bf3a08652d6228c",
+    "url": "/DubHacks/static/js/2.ac278706.chunk.js"
+  },
+  {
+    "revision": "d660a5eb8e578da025cc",
+    "url": "/DubHacks/static/js/main.80af0e0c.chunk.js"
+  },
+  {
+    "revision": "46edeb2dbc8732b87d83",
+    "url": "/DubHacks/static/js/runtime-main.5fbceb70.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/DubHacks/static/media/logo.25bf045c.svg"
+  }
+]);
